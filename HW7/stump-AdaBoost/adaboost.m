837ae848f77de74b54003e995c5f17bd3e9b3471
Zero Weight <zwt15@mails.tsinghua.edu.cn>
2017-05-02 17:48:35 +0800

    starting report w

--- HW7/stump-AdaBoost/adaboost.m
+++ HW7/stump-AdaBoost/adaboost.m
@@ -19,7 +19,7 @@ for m = 1: M
     
     trainErr(m) = adaboost_error(Xtrain, ytrain, j, a, d, c);
     testErr(m) = adaboost_error(Xtest, ytest, j, a, d, c);
-    fprintf( 'Error ratio of the decision stump: %.9f\nAdaBoost training error ratio %.9f\nAdaBoost testing ratio: %.9f\n\n', e, trainErr(m), testErr(m));
+    fprintf( 'Error ratio of the decision stump: %.9f\nAdaBoost training error ratio %.9f\nAdaBoost testing error ratio: %.9f\n\n', e, trainErr(m), testErr(m));
     itera(m,1)=m;
     itera(m,2)=trainErr(m);
     itera(m,3)=testErr(m);
